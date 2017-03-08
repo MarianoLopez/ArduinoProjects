73b5d0b1f16abce7271b600bfe07cc8a137bbe6d
Mariano <m_villa@hotmail.com>
2017-03-08 11:50:13 -0300

    servo test

---
+++ servo_test/servo_test.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+Servo myservo; // create servo object to control servo
+
+void setup() {
+  myservo.attach(3);// attach servo on pin 3 to servo object
+  Serial.begin(9600);     
+  Serial.setTimeout(500);
+}
+
+void loop() {
+  int aux;
+  if(Serial.available()>0){    
+    aux = Serial.readString().toInt();
+    Serial.print("Servo:");
+    Serial.println(aux);
+    myservo.write(aux);
+  }
+  delay(1000);
+}
+
+
+
+
+
