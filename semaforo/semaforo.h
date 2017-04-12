7219e465eccf6e7cff5e5028f25263813bc485c4
Mariano <m_villa@hotmail.com>
2017-04-12 13:18:12 -0300

    semaforo poo

---
+++ semaforo/semaforo.h
@@ -0,0 +1,27 @@
+class Semaforo{
+  private:
+    int rojo,amarillo,verde;
+    
+    void pinSetup(){
+      pinMode(rojo,OUTPUT);
+      pinMode(amarillo,OUTPUT);
+      pinMode(verde,OUTPUT);
+    }
+    
+  public:
+    //constructor
+    Semaforo(int r, int a, int v){
+      setRedLed(r);
+      setYellowLed(a);
+      setGreenLed(v);
+      pinSetup();
+    }
+    //setters
+    void setRedLed(int r){rojo = r;}
+    void setYellowLed(int a){amarillo = a;}
+    void setGreenLed(int v){verde = v;}
+    //getters
+    int getRedLed(){return rojo;}
+    int getYellowLed(){return amarillo;}
+    int getGreenLed(){return verde;}
+  };
--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -1,26 +1,30 @@
+#include "Semaforo.h"
+Semaforo semaforo(3,4,5);
+Semaforo semaforo2(8,9,10);
 
-
-const int rojo = 3;
-const int amarillo = 4;
-const int verde = 5;
-
-
-void setup() {
-  pinMode(rojo, OUTPUT);
-  pinMode(amarillo, OUTPUT);
-  pinMode(verde, OUTPUT);
-  }
+void setup() {}
 
 // the loop function runs over and over again forever
 void loop() {
-  semaforo();
+  semaforos();
 }
 
-void semaforo() {
-  luz(rojo, 25);
-  luz(amarillo, 5);
-  luz(verde, 25);
-  blinky(verde, 5);  
+void semaforos() {
+  /*luz(semaforo.getRedLed(), 25);
+  luz(semaforo.getYellowLed(), 5);
+  luz(semaforo.getGreenLed(), 25);
+  blinky(semaforo.getGreenLed(), 5);  
+  luz(semaforo.getYellowLed(), 5);*/
+
+  luzSync(semaforo.getRedLed(),semaforo2.getGreenLed(), 25);
+  blinkySync(semaforo2.getGreenLed(),semaforo.getRedLed(), 5);  
+  
+  luzSync(semaforo.getYellowLed(),semaforo2.getYellowLed(), 5);
+  
+  luzSync(semaforo.getGreenLed(),semaforo2.getRedLed(), 25);
+  blinkySync(semaforo.getGreenLed(),semaforo2.getRedLed(), 5);  
+  
+  luzSync(semaforo.getYellowLed(),semaforo2.getYellowLed(), 5);
 }
 
 void luz(int led, int tiempo) {
@@ -29,10 +33,25 @@ void luz(int led, int tiempo) {
   digitalWrite(led, LOW);
   }
 
+  void luzSync(int led,int led2, int tiempo) {
+  digitalWrite(led, HIGH);digitalWrite(led2, HIGH);
+  delay(tiempo*1000);
+  digitalWrite(led, LOW);digitalWrite(led2, LOW);
+  }
+
 
 void blinky(int led, int t)  {
   for (int i=0; i<t; i++) {
-  luz(led, 1);
-  delay(1000);
+    luz(led, 1);
+    delay(1000);
   }
+}
+
+void blinkySync(int led, int t,int led2)  {
+  digitalWrite(led2, HIGH);
+  for (int i=0; i<t; i++) {
+    luz(led, 1);
+    delay(1000);
+  }
+  digitalWrite(led2, LOW);
 }
