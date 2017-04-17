dca7e60b4e5e7a00b83d2208f8b0e9e1f07d3734
Mariano <m_villa@hotmail.com>
2017-04-17 13:46:44 -0300

    semaforos individuales

--- semaforo/semaforo.h
+++ semaforo/semaforo.h
@@ -2,40 +2,39 @@
 enum LED{RED_LED=0,YELLOW_LED=1,GREEN_LED=2};
 class Semaforo{
   private:
-    String nombre;
+    int id;
     Led leds[3];//red,yellow,green;
     bool message;//bandera para imprimir mensajes
+    
     //setters
-    void setNombre(String s){nombre=s;}
+    void setId(int s){id=s;}
     void setLed(LED num, String nombre, int pin) {leds[num]=Led(nombre,pin);}
+    
     //getters
     Led getLed(LED num){return leds[num];}
-    String getNombre(){return nombre;}
+    
     //imprimir mensaje
-    void printMessageIf(String message){
-      if(message){Serial.println(message);}  
-    }
+    void printMessageIf(String message){if(message){Serial.println(message);}}
     
   public:
-    //constructor
-    Semaforo(String n,int red, int yellow, int green){
-      setNombre(n);
+    //constructores
+    Semaforo(){}
+    Semaforo(int n,int red, int yellow, int green){
+      setId(n);
       setLed(RED_LED,"Rojo",red);
       setLed(YELLOW_LED,"Amarillo",yellow);
       setLed(GREEN_LED,"Verde",green);
       message = false;
     }
+    //getters
+    int getId(){return id;}
+    
     //funciones
     void turnLedOn(LED num){
-      for(int i=0;i<(sizeof(leds)/sizeof(int));i++){//sizeof returns the total number of bytes.
-        Led aux = getLed(num);
-        if(i==num){
-          aux.turnOn();//encender
-          printMessageIf(getNombre()+" "+aux.toString());
-        }else{
-          aux.turnOff();//apagar el resto
-        }
-      }
+      for(int i=RED_LED;i<=GREEN_LED;i++){leds[i].turnOff();}//apagar todos
+      Led aux = getLed(num);
+      aux.turnOn();
+      printMessageIf("Semaforo "+String(getId())+" - "+aux.toString());
     }
     void turnLedOff(LED num){getLed(num).turnOff();}
     void enableMessages(){message=true;}
--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -1,46 +1,92 @@
 #include "Semaforo.h"
-Semaforo semaforo("semaforo 1",3,4,5);
-Semaforo semaforo2("semaforo 2",8,9,10);
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+
+//                     Addr, En, Rw, Rs, d4, d5, d6, d7
+LiquidCrystal_I2C lcd( 0x27, 2, 1, 0, 4, 5, 6, 7);
+Semaforo semaforo,semaforo2,semaforo3,semaforo4;
 int contador;
+int semaforo_activo = 1;
+
+int RED_GREEN_SEC_WAIT = 23;
+int YELLOW_SEC_WAIT = 2;
+int GREEN_SEC_BLINK = 5;
 
 void setup() {
   Serial.begin(9600);
+  semaforo  = Semaforo(1,2,3,4);
+  semaforo2 = Semaforo(2,5,6,7);
+  semaforo3 = Semaforo(3,8,9,10);
+  semaforo4 = Semaforo(4,11,12,13);
   semaforo.enableMessages();//imprime los estados de los pins por serial
   semaforo2.enableMessages();
-  delay(1000);
+  semaforo3.enableMessages();
+  semaforo4.enableMessages();
+  initLCD();
+  semaforo.turnLedOn(RED_LED);
+  semaforo2.turnLedOn(RED_LED);
+  semaforo3.turnLedOn(RED_LED);
+  semaforo4.turnLedOn(RED_LED);
+  contador = RED_GREEN_SEC_WAIT+GREEN_SEC_BLINK+YELLOW_SEC_WAIT;
+}
+
+void turnSemaforo(Semaforo s,Semaforo prox){
+  semaforo_activo = s.getId();
+  s.turnLedOn(GREEN_LED);
+  wait(RED_GREEN_SEC_WAIT);//5
+  
+  blinky(s,GREEN_SEC_BLINK);//parpadeo + espera 5
+  
+  s.turnLedOn(YELLOW_LED);
+  prox.turnLedOn(YELLOW_LED);
+  wait(YELLOW_SEC_WAIT); //2
+  
+  s.turnLedOn(RED_LED);
+  contador = RED_GREEN_SEC_WAIT+GREEN_SEC_BLINK+YELLOW_SEC_WAIT;//sec
 }
 
 void loop() {
-  contador = 60;//1min
-  semaforo.turnLedOn(RED_LED);
-  semaforo2.turnLedOn(GREEN_LED);
-  wait(28);
-  
-  semaforo.turnLedOn(YELLOW_LED);
-  semaforo2.turnLedOn(YELLOW_LED);
-  wait(2);
-
-  semaforo.turnLedOn(GREEN_LED);
-  semaforo2.turnLedOn(RED_LED);
-  wait(23);
-  //parpadeo
-  for(int i=0;i<5;i++){
-    semaforo.turnLedOn(GREEN_LED);  
-    wait(1);
-    semaforo.turnLedOff(GREEN_LED);  
-  }
-  
-  semaforo.turnLedOn(YELLOW_LED);
-  semaforo2.turnLedOn(YELLOW_LED);
-  wait(2);
+  turnSemaforo(semaforo,semaforo2);
+  turnSemaforo(semaforo2,semaforo3);
+  turnSemaforo(semaforo3,semaforo4);
+  turnSemaforo(semaforo4,semaforo);
 }
 
+void blinky(Semaforo &s,int &sec){
+  for(int i=0;i<sec;i++){
+    refresh();
+    s.turnLedOn(GREEN_LED);  
+    delay(500);
+    s.turnLedOff(GREEN_LED);  
+    delay(500);
+  }
+}
 void wait(int secs){
   for(int i=0;i<secs;i++){
-    Serial.println(contador);
+    refresh();
     delay(1000);
-    contador-=1;
   }
 }
+
+void refresh(){
+  Serial.print("No Cruce Por: ");Serial.println("S"+String(semaforo_activo)+": "+String(contador));//Serial
+  lcd.clear();lcd.home();lcd.print("No Cruce Por: ");lcd.setCursor(0,1);lcd.print("S"+String(semaforo_activo)+": "+String(contador));//LCD
+  contador-=1;
+}
+
+
+void initLCD(){
+  lcd.begin (16,2);    // Inicializar el display con 16 caraceres 2 lineas
+ lcd.setBacklightPin(3,POSITIVE);
+ lcd.setBacklight(HIGH);
+ delay(10);
+ lcd.home ();                   // go home
+ lcd.print(" ");
+ lcd.setCursor ( 0, 1 );        // go to the 2nd line
+ lcd.print(" "); 
+}
+
+
 
 
