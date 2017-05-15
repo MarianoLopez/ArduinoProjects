53a419a45898f0d5d374a851bb19ba7b48a8f99a
Mariano <m_villa@hotmail.com>
2017-05-15 13:37:35 -0300

    car obj

---
+++ Car_object/Car_object.ino
@@ -0,0 +1,39 @@
+#include "MotorDriver.h"
+#include "Ultrasonico.h"
+MotorDriver driver;
+Ultrasonico ultrasonico;
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(100);
+  driver = MotorDriver(3,9,8,5,7,6);//MotorA,in1,in2,MotorB,in3,in4
+  //ultrasonico = Ultrasonico(A4,A5);//Echo,Trig
+}
+
+void loop() {
+  if(Serial.available()>0){
+    String s =Serial.readString();
+    Serial.println(s);
+    if(s=="f"){
+      driver.forward();
+    }
+    else if(s=="b"){
+      driver.back();
+    }
+    else if(s=="l"){
+      driver.left();
+    }
+    else if(s=="r"){
+      driver.right();
+    }
+    else if(s=="s"){
+       driver.stop();     
+    }else if(s.indexOf("speed=")!=-1){
+      String aux = s.substring(s.indexOf("="));
+      int _inicio = aux.indexOf("=");
+      driver.setSpeed(aux.substring(_inicio+1).toInt());
+    }else if(s=="u"){
+      //Serial.println(ultrasonico.getDistance());
+    }
+  }
+
+}
---
+++ Car_object/MotorDriver.h
@@ -0,0 +1,90 @@
+class MotorDriver{
+  private:
+    int motorA, motorB;
+    int in1,in2,in3,in4,_speed;
+    boolean mensajes;
+    void init(){
+      pinMode(in1,OUTPUT);pinMode(in2,OUTPUT);
+      pinMode(in3,OUTPUT);pinMode(in4,OUTPUT);
+      pinMode(motorA, OUTPUT);pinMode(motorB, OUTPUT);
+    }
+    void setMotorsSpeed(){
+       int aux = getSpeed();
+       if(_speed!=0){aux+=40;}//fix dx
+       printIf("Motor speed: "+getSpeed());
+       analogWrite(motorA,getSpeed());
+       analogWrite(motorB,aux);
+    }
+    void printIf(String message){if(mensajes){Serial.println(message);}}
+    
+    void motorForward(char motor){
+      if(motor=='A'){
+        digitalWrite(in1,HIGH);
+        digitalWrite(in2,LOW); 
+      }else{
+        digitalWrite(in3,HIGH);
+        digitalWrite(in4,LOW);  
+      }
+    }
+    
+    void motorBackward(char motor){
+      if(motor=='A'){
+        digitalWrite(in1,LOW);
+        digitalWrite(in2,HIGH);  
+      }else{
+        digitalWrite(in3,LOW);
+        digitalWrite(in4,HIGH);
+      } 
+    }
+
+  public:
+    MotorDriver(){}
+    MotorDriver(int enA,int i1,int i2,int enB,int i3,int i4){
+      motorA = enA;
+      motorB = enB;
+      in1 = i1;
+      in2 = i2;
+      in3 = i3;
+      in4 = i4;
+      _speed = 100;
+      mensajes = false;
+      init();
+    }
+    void enableMensajes(){mensajes = true;}
+    void disableMensajes(){mensajes = false;}
+    void setSpeed(int s){_speed = s;}
+    int getSpeed(){return _speed;}
+
+    void forward(){ 
+      setMotorsSpeed();
+      motorForward('A');
+      motorForward('B');
+      printIf("go forward!");
+    }
+    void back(){
+      setMotorsSpeed();
+      motorBackward('A');
+      motorBackward('B');
+      printIf("go back!");
+    }
+    void left(){
+      setMotorsSpeed();
+      motorBackward('B');
+      motorForward('A');
+      printIf("go left!");
+    }
+    void right(){
+      setMotorsSpeed();
+      motorForward('B');
+      motorBackward('A');
+      printIf("go right!");
+    }
+    void stop(){
+      int aux = getSpeed();
+      setSpeed(0);
+      setMotorsSpeed();
+      setSpeed(aux);
+      printIf("Stop!");
+    }
+};
+
